344ba587842b0deb5a318d4e8555c598eff1696b
Ayenidaniel <ayenimayokun@gmail.com>
2023-02-10 09:06:31 +0100

    MostPopular1 section uploaded

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'bac': "url('./img/bac.jpg')",
+        '1': "url('./img/1.jpg.webp')",
+        '2': "url('./img/2.jpg.webp')",
+        '3': "url('./img/3.jpg.webp')",
+        '4': "url('./img/4.jpg.webp')",
+
+        
+      }
+
+    },
     fontFamily: {
       'sans': ['Poppins', 'sans-serif'],
 
